41f7813809947cebfce1fb9b2a5a1735f649a203
Graham Wright <gwright99@hotmail.com>
2024-08-29 09:37:24 -0400

    Test: Adding App4.

---
+++ apps/app4/src/app.py
@@ -0,0 +1,19 @@
+def handler(event, context):
+    ''' Lambda App 3 '''
+    print("hello from ap3!")
+
+    expected_response = {
+        "statusCode": 201,
+        "headers": {
+            "Content-Type": "application/json",
+            "My-Custom-Header": "APP4"
+        },
+        "body": "{ \"message\": \"This is app4, after editing.\" }",
+
+        "isBase64Encoded": False
+    }
+
+    print("Hello from APP4")
+
+
+    return expected_response 
