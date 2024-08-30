de5aa51c682241140b461c6c5d292fcd7ecc37a1
Graham Wright <gwright99@hotmail.com>
2024-08-30 00:14:17 -0400

    App5 test.

---
+++ apps/app5/src/app.py
@@ -0,0 +1,19 @@
+def handler(event, context):
+    ''' Lambda App 3 '''
+    print("hello from app5!")
+
+    expected_response = {
+        "statusCode": 201,
+        "headers": {
+            "Content-Type": "application/json",
+            "My-Custom-Header": "AP5"
+        },
+        "body": "{ \"message\": \"This is app4, after editing.\" }",
+
+        "isBase64Encoded": False
+    }
+
+    print("Hello from APP5")
+
+
+    return expected_response 
